9b465e596023f2b075ab66cd2668dc3bc6da6901
Ritesh DevOps <riteshagarwal1497@gmail.com>
2023-09-19 15:28:04 +0530

    Modified

---
+++ Functions & Recursion/Function.py
@@ -0,0 +1,5 @@
+'''Function is a group of statement performing specific tasks.'''
+
+'''This states that if you want to perform multiple tasks and wants the output, you can consolidate it
+in one funtion and print it multiple times just by changing the values.
+'''
